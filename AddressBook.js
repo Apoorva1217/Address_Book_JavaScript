23ecc7a4624b872db0886b095796d3d73e8d67c7
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-08 17:45:09 +0530

    [Apoorva] Added : Ability to ensure Valid Contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -14,27 +14,79 @@ class AddressBook {
         this.emailId = params[7];
     }
 
+    //UC2 Ability to ensure Valid Contacts are added
     get firstName() { return this._firstName; }
-    set firstName(firstName) { return this._firstName; }
+    set firstName(firstName) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else throw "First Name is should have minimum 3 character with first letter Capital!";
+    }
 
     get lastName() { return this._lastName; }
-    set lastName(lastName) { return this._lastName; }
+    set lastName(lastName) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else throw "Last Name is should have minimum 3 character with first letter Capital!";
+    }
 
     get address() { return this._address; }
-    set address(address) { return this._address; }
+    set address(address) {
+        let addressRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (addressRegex.test(address))
+            this._address = address;
+        else throw "Address is should have minimum 4 character with first letter Capital!";
+    }
 
     get city() { return this._city; }
-    set city(city) { return this._city; }
+    set city(city) {
+        let cityRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (cityRegex.test(city))
+            this._city = city;
+        else throw "City is should have minimum 3 character with first letter Capital!";
+    }
 
     get state() { return this._state; }
-    set state(state) { return this._state; }
-
+    set state(state) {
+        let stateRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (stateRegex.test(state))
+            this._state = state;
+        else throw "State is should have minimum 3 character with first letter Capital!";
+    }
     get zipCode() { return this._zipCode; }
-    set zipCode(zipCode) { return this._zipCode; }
+    set zipCode(zipCode) {
+        let zipRegex = RegExp('^[0-9]{6}$');
+        if (zipRegex.test(zipCode))
+            this._zipCode = zipCode;
+        else throw "ZipCode must have 6 digits!";
+    }
 
     get phoneNumber() { return this._phoneNumber; }
-    set phoneNumber(phoneNumber) { return this._phoneNumber; }
+    set phoneNumber(phoneNumber) {
+        let phoneRegex = RegExp('^[0-9]{10}');
+        if (phoneRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else throw "Invalid Phone Number!";
+    }
 
     get emailId() { return this._emailId; }
-    set emailId(emailId) { return this._emailId; }
+    set emailId(emailId) {
+        let mailRegex = RegExp('^[0-9a-zA-Z]+([+_.-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$');
+        if (mailRegex.test(emailId))
+            this._emailId = emailId;
+        else throw "Invalid Email ID!";
+    }
+
+    toString() {
+        return "\n  FirstName    : " + this.firstName
+             + "\n  LastName     : " + this.lastName
+             + "\n  Address      : " + this.address
+             + "\n  City         : " + this.city
+             + "\n  State        : " + this.state
+             + "\n  ZipCode      : " + this.zipCode
+             + "\n  MobileNumber : " + this.mobileNumber
+             + "\n  EmailID      : " + this.emailID + "\n" +
+             "/****************************************/";
+    }
 }
